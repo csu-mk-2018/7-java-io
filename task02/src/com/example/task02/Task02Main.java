4a5ae65ef68d704bdf321fbcc2171505f04e0445
Leva <petrosian.lyova@yandex.ru>
2018-12-19 00:49:43 +0500

    Петросян. Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,25 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        convertWin2Unix(System.in, System.out);
+    }
+
+    private static void convertWin2Unix(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int currentByte = inputStream.read();
+        int nextByte;
+
+        while (currentByte >= 0) {
+            nextByte = inputStream.read();
+            if (!(nextByte == 10 && currentByte == 13)) { //: /r/n
+                outputStream.write(currentByte);
+            }
+            currentByte = nextByte;
+        }
+        outputStream.flush();
     }
 }
