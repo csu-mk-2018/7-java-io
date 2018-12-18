822e5a01b25c25d3152c534674b47029483d9185
Leva <petrosian.lyova@yandex.ru>
2018-12-19 01:29:21 +0500

    Петросян. Task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,19 +3,34 @@ package com.example.task03;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.io.*;
 
 public class Task03Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
-        System.out.println(readAsString(new FileInputStream("task03/src/com/example/task03/input.test"), Charset.forName("KOI8-R")));
-        */
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(new byte[]{48, 49, 50, 51});
+        String str = readAsString(inputStream, Charset.forName("ASCII"));
+        System.out.println(str);
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        String retval;
+        StringWriter r = new StringWriter();
+        int b;
+
+        try {
+            Reader reader = new InputStreamReader(inputStream, charset);
+
+            while ((b = reader.read()) != -1) {
+                r.write(b);
+            }
+
+            retval = r.toString();
+            return retval;
+        } catch (Exception e) {
+            if (e instanceof IOException)
+                throw new IOException();
+            else
+                throw new IllegalArgumentException();
+        }
     }
 }
