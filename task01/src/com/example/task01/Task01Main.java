455d0fc7ae3ba3f39f4ea677c7fbcf6215d8ee87
Vadim Metnyov <metnyov@gmail.com>
2018-12-20 17:54:01 +0500

    task1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -15,7 +15,15 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        if (inputStream == null) {
+            throw new IllegalArgumentException();
+        }
+        int checkSum = 0;
+        int b = inputStream.read();
+        while (b >= 0) {
+            checkSum = Integer.rotateLeft(checkSum, 1) ^ b;
+            b = inputStream.read();
+        }
+        return checkSum;
     }
 }
