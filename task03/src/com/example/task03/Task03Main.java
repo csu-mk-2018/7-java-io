e7d6d68d307d28ea9dff630245d66c2e4ba90b70
Nikita Sviridov <n.sviridov14@gmail.com>
2018-12-23 13:25:48 +0500

    Sviridov Nikita task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,6 @@
 package com.example.task03;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 
 public class Task03Main {
@@ -15,7 +14,24 @@ public class Task03Main {
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
-        // your implementation here
-        return "";
+        String retValue;
+        StringWriter sWriter = new StringWriter();
+        int cByte;
+
+        try {
+            Reader reader = new InputStreamReader(inputStream, charset);
+
+            while ((cByte = reader.read()) != -1) {
+                sWriter.write(cByte);
+            }
+
+            retValue = sWriter.toString();
+            return retValue;
+        } catch (Exception e) {
+            if (e instanceof IOException)
+                throw new IOException();
+            else
+                throw new IllegalArgumentException();
+        }
     }
 }
