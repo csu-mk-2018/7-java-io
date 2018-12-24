dae141eda855a189730e4124dd0bc631771fb31e
BMSalamander <hostan3@gmail.com>
2018-12-24 20:02:11 +0500

    Антипьев Александр. Задание 02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,16 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int currentByte;
+        int nextByte;
+
+        currentByte = System.in.read();
+        while (currentByte != -1) {
+            nextByte = System.in.read();
+            if (currentByte != '\r' || nextByte != '\n') {
+                System.out.write(currentByte);
+            }
+            currentByte = nextByte;
+        }
     }
 }
