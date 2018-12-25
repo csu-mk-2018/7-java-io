55efc4f53fdfd0eb0e7e1510b48e76ebd9244d42
DanilaKroshin <skozlozopsk74@gmail.com>
2018-12-25 09:01:02 +0500

    Kroshin task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,8 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +11,20 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        convertToUnix(System.in, System.out);
+    }
+
+    private static void convertToUnix(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int cByte = inputStream.read();
+        int nextByte;
+
+        while (cByte >= 0) {
+            nextByte = inputStream.read();
+            if (!(nextByte == 10 && cByte == 13)) {
+                outputStream.write(cByte);
+            }
+            cByte = nextByte;
+        }
+        outputStream.flush();
     }
 }
