1018cd66eb793c3fc78fc6f24ebc679b8587db5f
Nicola3009 <polozov97@mail.ru>
2018-12-25 10:31:26 +0500

    Полозов Николай. task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,20 +2,29 @@ package com.example.task01;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
 
-        /*
-        System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
+       // System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        int checktmp = 0;
+        int Byte;
+
+        try {
+            while ((Byte = inputStream.read()) != -1) {
+                checktmp = Integer.rotateLeft(checktmp, 1) ^ Byte;
+            }
+        } catch (Exception excep) {
+            if (excep instanceof IOException)
+                throw excep;
+            else
+                throw new IllegalArgumentException();
+        }
+
+        return checktmp;
     }
 }
